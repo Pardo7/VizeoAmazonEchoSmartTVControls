aa3b25b0c379e6f0b9f62a3c15eec26536403b03
Pardo7 <walterpardo7@gmail.com>
2018-01-02 15:35:54 -0500

    Implemented volume controls

--- server.js
+++ server.js
@@ -325,3 +325,151 @@ app.post('/inputthree', (request, response) => {
 
 	});
 });
+
+app.post('/volumeup', (request, response) => {
+	let requestBody = "";
+
+	// Will accumilate the date
+	request.on('data', data => {
+		requestBody += data;
+	});
+
+	// Called when all data has been accumilated
+	request.on('end', () => {
+		let responseBody = {};
+
+		// Parsing the request body for information
+		let jsonData = JSON.parse(requestBody);
+		// console.log(jsonData);
+		if (jsonData.request.type == 'LaunchRequest') {
+
+			// crafting a response
+			responseBody = {
+				"version": "0.1",
+				"response": {
+					"outputSpeech": {
+						"type": "PlainText",
+						"text": "Volume adjusted up."
+					},
+					"card": {
+						"type": "Simple",
+						"title": "Opened",
+						"content": "You have started the Sudo AI Node Control"
+					},
+					"reprompt": {
+						"outputSpeech": {
+							"type": "PlainText",
+							"text": "Say a command"
+						}
+					},
+					"shouldEndSession": true
+				}
+			};
+
+			tv.control.volume.up();
+
+		} else {
+			// Not a recognized type
+			responseBody = {
+				"version": "0.1",
+				"response": {
+					"outputSpeech": {
+						"type": "PlainText",
+						"text": "Could not parse data"
+					},
+					"card": {
+						"type": "Simple",
+						"title": "Error Parsing",
+						"content": JSON.stringify(requestBody)
+					},
+					"reprompt": {
+						"outputSpeech": {
+							"type": "PlainText",
+							"text": "Say a command"
+						}
+					},
+					"shouldEndSession": true
+				}
+			};
+		}
+
+		response.statusCode = 200;
+		response.contentType('application/json');
+		response.send(responseBody);
+
+	});
+});
+
+app.post('/volumedown', (request, response) => {
+	let requestBody = "";
+
+	// Will accumilate the date
+	request.on('data', data => {
+		requestBody += data;
+	});
+
+	// Called when all data has been accumilated
+	request.on('end', () => {
+		let responseBody = {};
+
+		// Parsing the request body for information
+		let jsonData = JSON.parse(requestBody);
+		// console.log(jsonData);
+		if (jsonData.request.type == 'LaunchRequest') {
+
+			// crafting a response
+			responseBody = {
+				"version": "0.1",
+				"response": {
+					"outputSpeech": {
+						"type": "PlainText",
+						"text": "Volume adjusted down."
+					},
+					"card": {
+						"type": "Simple",
+						"title": "Opened",
+						"content": "You have started the Sudo AI Node Control"
+					},
+					"reprompt": {
+						"outputSpeech": {
+							"type": "PlainText",
+							"text": "Say a command"
+						}
+					},
+					"shouldEndSession": true
+				}
+			};
+
+			tv.control.volume.down();
+
+		} else {
+			// Not a recognized type
+			responseBody = {
+				"version": "0.1",
+				"response": {
+					"outputSpeech": {
+						"type": "PlainText",
+						"text": "Could not parse data"
+					},
+					"card": {
+						"type": "Simple",
+						"title": "Error Parsing",
+						"content": JSON.stringify(requestBody)
+					},
+					"reprompt": {
+						"outputSpeech": {
+							"type": "PlainText",
+							"text": "Say a command"
+						}
+					},
+					"shouldEndSession": true
+				}
+			};
+		}
+
+		response.statusCode = 200;
+		response.contentType('application/json');
+		response.send(responseBody);
+
+	});
+});
